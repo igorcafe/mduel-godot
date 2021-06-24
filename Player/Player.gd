58d26b0d678e57ea1e2f69fe8da44ce7d612bebc
Igor Melo <igoracmelo@protonmail.com>
2021-06-24 13:17:54 -0300

    Better state handling

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,7 +9,8 @@ export var facing_right = true
 export var default_gravity = 10
 export var player_texture: Texture
 
-
+enum States { GROUND, AIR, ROPE }
+var generic_state = States.ROPE
 var gravity = default_gravity
 var vel = Vector2.ZERO
 var keys = {}
@@ -23,6 +24,17 @@ var jp_down := false
 var p_left := false
 var p_right := false
 var jp_item := false
+
+# Other flags
+var jumping := false
+var can_move_x := true
+var on_floor := false
+var can_jump := false
+var has_jumped := false
+var being_dragged := false
+var touching_rope := false
+var travel_to
+
 onready var anim_tree = $AnimationTree
 onready var coll_shape = $CollisionShape2D
 onready var state_machine = anim_tree["parameters/playback"]
@@ -43,28 +55,84 @@ func _physics_process(_delta):
 	p_right = Input.is_action_pressed(id + '_right')
 	jp_item = Input.is_action_just_pressed(id + '_item')
 	on_floor = is_on_floor()
-		
-	if is_on_floor():
-		if state() in ["idle", "run"]:
-			vel.x = move_speed * h_move
-		state_machine.travel("idle")
-		if vel.x and state() in ["idle", "run"]:
-			run()
-		if just_pressed(keys['up']):
-			jump()
-		if just_pressed(keys['item']):
-			use_item()
-		if pressed(keys['down']) and state() in ["duck", "idle"]:
-			duck()
-		elif just_pressed(keys['down']):
-			state_machine.travel("roll_forward")
+
+	match generic_state:
+		States.GROUND:
+			if not on_floor:
+				generic_state = States.AIR
+				return
+			
+			being_dragged = false
+			travel_to = "idle"
+			can_move_x = state() in [ "idle", "run" ]
+			can_jump = can_move_x
+
+			if can_move_x:
+				vel.x = 0
+				travel_to = "idle"
+				if p_right:
+					vel.x = +move_speed
+					travel_to = "run"
+					flip_h()
+				elif p_left:
+					vel.x = -move_speed
+					travel_to = "run"
+					flip_h()
+
+			if touching_rope and not vel.x and (p_up or p_down):
+				generic_state = States.ROPE
+				travel_to = "rope_climbing"
+			else:
+				if jp_up and can_jump and not has_jumped:
+					can_move_x = false
+					has_jumped = true
+					jump()
+					generic_state = States.AIR
+				elif jp_down and vel.x:
+					can_move_x = false
+					can_jump = false
+					travel_to = "roll_forward"
+				elif p_down and not vel.x:
+					can_move_x = false
+					can_jump = false
+					travel_to = "duck"
+			state_machine.travel(travel_to)
+
+		States.AIR:
+			if on_floor:
+				has_jumped = false
+				generic_state = States.GROUND
+				state_machine.travel("idle")
+
+			elif not being_dragged:
+				if has_jumped and vel.y < 0:
+					if vel.x:
+						travel_to = "jump_running"
+					else:
+						travel_to = "jump_in_place"
+				else:
+					travel_to = "fall"
+				state_machine.travel(travel_to)
 	
-	elif state() in ["run", "idle"]:
-		state_machine.travel("fall")
-	
-	elif state() in ["rope_idle", "rope_climbing"]:
-		vel.x = move_speed * h_move
-		rope()
+		States.ROPE:
+			set_collision_mask_bit(1, false)
+			set_collision_mask_bit(3, true)
+			gravity = 0
+			
+			vel.x = (int(p_right) - int(p_left)) * rope_speed
+			if vel.x or not touching_rope:
+				set_collision_mask_bit(1, true)
+				set_collision_mask_bit(3, false)
+				gravity = default_gravity
+				state_machine.travel("idle")
+				generic_state = States.GROUND
+			else:
+				vel.y = (int(p_down) - int(p_up)) * rope_speed
+				if vel.y:
+					travel_to = "rope_climbing"
+				else:
+					travel_to = "rope_idle"
+				state_machine.travel(travel_to)
 		
 	vel.y += gravity
 	vel = move_and_slide(vel, Vector2.UP)
@@ -79,49 +147,11 @@ func flip_h(value = null):
 	player_sprite.flip_h = value
 	boots_sprite.flip_h = value
 		
-func run():
-	state_machine.travel("run")
-	flip_h()
-#	else:
-#	$BootsSprite.flip_h = $Sprite.flip_h
-
-
 func duck():
 	vel.x = 0
 	state_machine.travel("duck")
 
-
-func set_coll_disabled(value):
-	coll_shape.disabled = value
-	
-func get_coll_disabled():
-	return coll_shape.disabled
-
-
-func rope():
-	set_collision_mask_bit(1, false)
-	gravity = 0
-	if vel.x:
-		set_collision_mask_bit(1, true)
-		gravity = default_gravity
-		state_machine.travel("idle")
-	elif pressed(keys['up']):
-		vel.y = -rope_speed
-		state_machine.travel("rope_climbing")
-	elif pressed(keys['down']):
-		vel.y = +rope_speed
-		state_machine.travel("rope_climbing")
-	else:
-		vel.y = 0
-		state_machine.travel("rope_idle")
-
 func jump(multi = 1):
-	if not state() in ["run", "idle"]:
-		return
-	if not vel.x:
-		state_machine.travel("jump_in_place")
-	else:
-		state_machine.travel("jump_running")
 	vel.y = -jump_speed * multi
 
 func state():
@@ -137,19 +167,7 @@ func roll_backwards():
 	
 func thrown_backwards():
 	vel.x = -vel.x
+	being_dragged = true
 	state_machine.travel("thrown_backward")
-
-func _on_ScreenBoundaries_body_entered(body):
-	thrown_backwards()
-
 func drown():
 	print("drown")
-
-func pressed(key):
-	return Input.is_action_pressed(key)
-	
-func just_pressed(key):
-	return Input.is_action_just_pressed(key)
-	
-func strength(key):
-	return Input.get_action_strength(key)
