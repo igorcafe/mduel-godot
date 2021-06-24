fd5e8d1841582fc4b5c2cec3e314798c55ba338d
Igor Melo <igoracmelo@protonmail.com>
2021-06-24 13:21:45 -0300

    Fixed Rope not working

---
+++ Environment/Rope/Rope.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _on_Rope_body_entered(body: Node) -> void:
+	var player = body as Player
+	player.touching_rope = true
+	
+func _on_Rope_body_exited(body: Node) -> void:
+	var player = body as Player
+	player.touching_rope = false
